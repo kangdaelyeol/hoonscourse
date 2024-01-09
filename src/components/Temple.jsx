e411fa656a07e97d17fd099d1f69bc53806d6cb1
rkdeofuf <asa47972000@naver.com>
2024-01-09 12:05:49 +0900

    add Temple canvas interaction

--- src/components/Temple.jsx
+++ src/components/Temple.jsx
@@ -7,25 +7,88 @@ import img2 from '../imgs/img2.png'
 
 const imgs = [img1, img2]
 
+const drawImageInCanvas = (ctx, ratio, motion, image) => {
+    ctx.clearRect(0, 0, 360, 800)
+    const boundary = (motion.paintRatio[1] + motion.removeRatio[0]) / 2
+
+    // 이미지 그려야할 x, y 구하기 칠하기
+    const cX = motion.paintX[1] - motion.paintX[0]
+    const cY = motion.paintY[1] - motion.paintY[0]
+    const cRange = motion.paintRatio[1] - motion.paintRatio[0]
+    const rRange = motion.removeRatio[1] - motion.removeRatio[0]
+
+    let pR = (ratio - motion.paintRatio[0]) / cRange
+    // 현재 진행 씬 ratio - 사라질 모션이 시작되는 ratio 시점 / 사라지는 모션의 범위
+    let rRatio = (ratio - motion.removeRatio[0]) / rRange
+
+    if (pR < 0) pR = 0
+    if (pR > 1) pR = 1
+    if (rRatio < 0) rRatio = 0
+    if (rRatio > 1) rRatio = 1
+
+    const pX = motion.paintX[0] + cX * pR
+    const pY = motion.paintY[0] + cY * pR
+
+    if (ratio < boundary) {
+        // Draw In
+        ctx.drawImage(image, 0, 0, pR * 1024, 1024, 0, 0, pX, 800)
+    } else {
+        // DrawOut
+        ctx.drawImage(
+            image,
+            rRatio * 1024,
+            0,
+            1024 - rRatio * 1024,
+            1024,
+            360 * rRatio,
+            0,
+            360 - 360 * rRatio,
+            800,
+        )
+    }
+}
+
+const imgMotion = {
+    paintX: [0, 360],
+    paintY: [100, 800],
+    paintRatio: [0, 0.2],
+    removeRatio: [0.3, 0.5],
+}
+
 export default function Temple() {
     const [imgInfo, setImgInfo] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const cRef = useRef()
     const wRef = useRef()
-    const s1Ref = useRef()
-
-    const onTemScroll = (e) => {}
+    const sRef = useRef()
 
     useEffect(() => {
-        if (!cRef || !wRef || !s1Ref || isLoading) return
-        console.log(imgInfo)
-        const vHeight = wRef.current.scrollHeight
-        const vWidth = wRef.current.scrollWidth
-        cRef.current.width = vWidth
-        cRef.current.height = vHeight
+        if (!cRef || !wRef || !sRef || isLoading || imgInfo.length !== 2) return
         const ctx = cRef.current.getContext('2d')
-        ctx.drawImage(imgInfo[0]?.img, 0, 0, 360, 600)
-    }, [cRef, wRef, s1Ref, isLoading, imgInfo])
+        const wrapCurrent = wRef.current
+
+        const onWrapScroll = (e) => {
+            const scrolledY =
+                e.currentTarget.getBoundingClientRect().top -
+                sRef.current.getBoundingClientRect().top
+
+            const scrolledRatio = scrolledY / sRef.current.offsetHeight
+
+            drawImageInCanvas(ctx, scrolledRatio, imgMotion, imgInfo[0]?.img)
+        }
+
+        // const vHeight = wRef.current.scrollHeight
+        // const vWidth = wRef.current.scrollWidth
+        // cRef.current.width = vWidth
+        // cRef.current.height = vHeight
+
+        wrapCurrent.addEventListener('scroll', onWrapScroll)
+
+        return () => {
+            wrapCurrent.addEventListener('scroll', onWrapScroll)
+        }
+        // ctx.drawImage(imgInfo[0]?.img, 0, 0, 360, 800)
+    }, [cRef, wRef, sRef, isLoading, imgInfo])
 
     useEffect(() => {
         const onWindowLoad = () => {
@@ -34,17 +97,17 @@ export default function Temple() {
         imgs.forEach((src) => {
             const img = new Image()
             img.src = src
-            img.onload = (e) => {
+            img.addEventListener('load', (e) => {
                 setImgInfo((v) => [
                     ...v,
                     {
                         img: e.target,
-                        src: src,
+                        src: img.src,
                         width: img.naturalWidth,
                         height: img.naturalHeight,
                     },
                 ])
-            }
+            })
         })
         window.addEventListener('load', onWindowLoad)
 
@@ -59,13 +122,16 @@ export default function Temple() {
                 {isLoading ? (
                     'Loading...'
                 ) : (
-                    <div
-                        ref={s1Ref}
-                        className={Styles.s1}
-                        onScroll={onTemScroll}
-                    >
-                        <canvas className={Styles.mycanvas} ref={cRef}></canvas>
-                    </div>
+                    <>
+                        <div ref={sRef} className={Styles.scene}>
+                            <canvas
+                                className={Styles.mycanvas}
+                                ref={cRef}
+                                width={360}
+                                height={800}
+                            ></canvas>
+                        </div>
+                    </>
                 )}
             </div>
         </div>
